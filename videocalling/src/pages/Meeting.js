45427469fa6bb95a4563b5c009c957462fc460c6
Shivansh Goel <shivanshgoel21@gmail.com>
2021-10-15 22:39:33 +0530

    Meeting.js: peer setup

--- videocalling/src/pages/Meeting.js
+++ videocalling/src/pages/Meeting.js
@@ -2,10 +2,12 @@ import React, { useEffect, useState, useRef } from 'react'
 import {
     useParams
 } from "react-router-dom";
-import {io} from 'socket.io-client';
+import { io } from 'socket.io-client';
 import { useHistory } from "react-router-dom";
+import Peer from 'peerjs';
 
 const socket = io('http://localhost:5000/')
+
 
 function Meeting() {
 
@@ -32,10 +34,10 @@ function Meeting() {
         let cons = setConstraints()
         return await navigator.mediaDevices.getUserMedia(cons);
     }
-    
 
-    useEffect(async ()=>{
-        if(socket.connected === false){
+
+    useEffect(async () => {
+        if (socket.connected === false) {
             history.push(`/`)
             alert("Couldn't connect to socket server at the movement")
             return
@@ -43,17 +45,53 @@ function Meeting() {
 
         let n = window.prompt('Enter Name')
         setName(n)
-        
-        socket.emit("joinroom", { name: n, room: id, peerId: 22 });
 
-        socket.on("addpartcipant", (data) => {
-            console.log('New Participant ', data)
-        });
+        socket.emit("joinroom", { name: n, room: id, peerId: 22 });
 
         let myStreamGlobal = await openStream()
         myVideo.current.srcObject = myStreamGlobal
 
-    },[])
+        let peer = new Peer(n)
+
+        // listen for incoming calls
+        peer.on('call', call => {
+
+            // metadata from call
+
+            // send you stream
+            call.answer(myStreamGlobal)
+
+            // append user stream to grid
+            call.on('stream', (stream) => {
+                console.log('Getting Peers stream')
+            })
+
+        })
+
+        socket.on("addpartcipant", (data) => {
+            console.log('New Participant ', data)
+            connectPeers(data.name)
+        });
+
+        // when someone joins our room, call them using their peer id
+        function connectPeers(name) {
+            // send userId in metadata of call
+            let options = { metadata: { "name": name } }
+
+            const call = peer.call(name, myStreamGlobal, options)
+
+            call.on('stream', userVideoStream => {
+                console.log('Getting User video stream')
+            }
+            )
+
+            // close video
+            call.on('close', () => {
+                console.log('User Left')
+            })
+        }
+
+    }, [])
 
     return (
         <div>
